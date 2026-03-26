f70c1ec289cc8095956fe373cd3e2f38741af958
Fahad-smto <simanto.smto.prog@gmail.com>
2026-03-27 00:33:37 +0600

    fix vercel

--- src/server.ts
+++ src/server.ts
@@ -21,5 +21,8 @@ app.use("/api/v1/vehicles", vehiclesRoute);
 app.use("/api/v1/bookings", bookingsRoute);
 
 app.get("/", (req, res) => res.send("Vehicle Rental API Running"));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(5000, () => console.log("Server running on port 5000"));
+}
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+export default app;
